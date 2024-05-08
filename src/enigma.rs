61d107abfe134d1dbd5b76f0da0d53a1b925a04d
Aki Tuomi <cmouse@cmouse.fi>
2024-05-08 14:58:48 +0300

    Polish code

--- src/enigma.rs
+++ src/enigma.rs
@@ -1,7 +1,7 @@
-use std::fs;
 use std::collections::HashMap;
 use std::env;
 use std::fmt;
+use std::fs;
 use std::io::{self, BufRead};
 
 extern crate yaml_rust;
@@ -10,219 +10,245 @@ use yaml_rust::{Yaml, YamlLoader};
 const ALPHA: &str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 /* name, wirings, notch(es), fixed */
-static WHEELS: &'static[(&str, &str, &str, bool)] = &[
-  ("IC", "DMTWSILRUYQNKFEJCAZBPGXOHV", "Q", false),
-  ("IIC", "HQZGPJTMOBLNCIFDYAWVEUSRKX", "E", false),
-  ("IIIC", "UQNTLSZFMREHDPXKIBVYGJCWOA", "V", false),
-  ("I", "JGDQOXUSCAMIFRVTPNEWKBLZYH", "Q", false),
-  ("II", "NTZPSFBOKMWRCJDIVLAEYUXHGQ", "E", false),
-  ("III", "JVIUBHTCDYAKEQZPOSGXNRMWFL", "V", false),
-  ("IV", "ESOVPZJAYQUIRHXLNFTGKDCMWB", "J", false),
-  ("V", "VZBRGITYUPSDNHLXAWMJQOFECK", "Z", false),
-  ("VI", "JPGVOUMFYQBENHZRDKASXLICTW", "ZM", false),
-  ("VII", "NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM", false),
-  ("VIII", "FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM", false),
-
-  ("Reflector A", "EJMZALYXVBWFCRQUONTSPIKHGD", "", true),
-  ("Reflector B", "YRUHQSLDPXNGOKMIEBFZCWVJAT", "", true),
-  ("Reflector C", "FVPJIAOYEDRZXWGCTKUQSBNMHL", "", true),
-  ("Reflector B Thin", "ENKQAUYWJICOPBLMDXZVFTHRGS", "", true),
-  ("Reflector C Thin", "RDOBJNTKVEHMLFCWZAXGYIPSUQ", "", true),
+static WHEELS: &'static [(&str, &str, &str, bool)] = &[
+    ("IC", "DMTWSILRUYQNKFEJCAZBPGXOHV", "Q", false),
+    ("IIC", "HQZGPJTMOBLNCIFDYAWVEUSRKX", "E", false),
+    ("IIIC", "UQNTLSZFMREHDPXKIBVYGJCWOA", "V", false),
+    ("I", "JGDQOXUSCAMIFRVTPNEWKBLZYH", "Q", false),
+    ("II", "NTZPSFBOKMWRCJDIVLAEYUXHGQ", "E", false),
+    ("III", "JVIUBHTCDYAKEQZPOSGXNRMWFL", "V", false),
+    ("IV", "ESOVPZJAYQUIRHXLNFTGKDCMWB", "J", false),
+    ("V", "VZBRGITYUPSDNHLXAWMJQOFECK", "Z", false),
+    ("VI", "JPGVOUMFYQBENHZRDKASXLICTW", "ZM", false),
+    ("VII", "NZJHGRCXMYSWBOUFAIVLPEKQDT", "ZM", false),
+    ("VIII", "FKQHTLXOCBJSPDZRAMEWNIUYGV", "ZM", false),
+    ("Reflector A", "EJMZALYXVBWFCRQUONTSPIKHGD", "", true),
+    ("Reflector B", "YRUHQSLDPXNGOKMIEBFZCWVJAT", "", true),
+    ("Reflector C", "FVPJIAOYEDRZXWGCTKUQSBNMHL", "", true),
+    ("Reflector B Thin", "ENKQAUYWJICOPBLMDXZVFTHRGS", "", true),
+    ("Reflector C Thin", "RDOBJNTKVEHMLFCWZAXGYIPSUQ", "", true),
 ];
 
 struct Wheel {
-  _name: String,
-  wiring: String,
-  step: usize,
-  notch: String,
-  fixed: bool,
+    _name: String,
+    wiring: String,
+    step: usize,
+    notch: String,
+    fixed: bool,
 }
 
 #[derive(Debug)]
-struct WheelNotFound {
-}
+struct WheelNotFound {}
 
 impl std::fmt::Display for Wheel {
-   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-     write!(f, "{} at step {}", self._name, self.step)
-   }
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{} at step {}", self._name, self.step)
+    }
 }
 
 impl Wheel {
-  pub fn new(name: &str, wiring: &str, notch: &str, fixed: bool) -> Wheel {
-    return Wheel { _name: name.to_string(), wiring: wiring.to_string(), step: 0, notch: notch.to_string(), fixed: fixed } ;
-  }
-
-  pub fn by_name(name: &str) -> Result<Wheel, WheelNotFound> {
-     for set in WHEELS {
-       if set.0 == name {
-         return Ok(Wheel::new(name, set.1, set.2, set.3));
-       }
-     }
-     Err(WheelNotFound {})
-  }
-
-  fn modulo(n: i32, m: i32) -> i32 {
-    ((n % m) + m) % m
-  }
-
-  pub fn send_right(&self, c: char) -> char {
-    match ALPHA.find(c) {
-      Some(index) => {
-        let new_index = Wheel::modulo(index as i32 + self.step as i32, self.wiring.len() as i32);
-        return self.wiring.chars().nth(new_index as usize).unwrap();
-      },
-      None => panic!("Letter encoding missing!")
+    pub fn new(name: &str, wiring: &str, notch: &str, fixed: bool) -> Wheel {
+        Wheel {
+            _name: name.to_string(),
+            wiring: wiring.to_string(),
+            step: 0,
+            notch: notch.to_string(),
+            fixed: fixed,
+        }
     }
-  }
 
-  pub fn send_left(&self, c: char) -> char {
-    match self.wiring.find(c) {
-      Some(index) => {
-        let new_index = Wheel::modulo(index as i32 - self.step as i32, ALPHA.len() as i32);
-        return ALPHA.chars().nth(new_index.abs() as usize).unwrap();
-      },
-      None => panic!("Letter encoding missing!")
+    pub fn by_name(name: &str) -> Result<Wheel, WheelNotFound> {
+        for set in WHEELS {
+            if set.0 == name {
+                return Ok(Wheel::new(name, set.1, set.2, set.3));
+            }
+        }
+        Err(WheelNotFound {})
     }
-  }
+
+    fn modulo(n: i32, m: i32) -> i32 {
+        ((n % m) + m) % m
+    }
+
+    pub fn send_right(&self, c: char) -> char {
+        match ALPHA.find(c) {
+            Some(index) => {
+                let new_index =
+                    Wheel::modulo(index as i32 + self.step as i32, self.wiring.len() as i32);
+                return self.wiring.chars().nth(new_index as usize).unwrap();
+            }
+            None => panic!("Letter encoding missing!"),
+        }
+    }
+
+    pub fn send_left(&self, c: char) -> char {
+        match self.wiring.find(c) {
+            Some(index) => {
+                let new_index = Wheel::modulo(index as i32 - self.step as i32, ALPHA.len() as i32);
+                return ALPHA.chars().nth(new_index.abs() as usize).unwrap();
+            }
+            None => panic!("Letter encoding missing!"),
+        }
+    }
 }
 
 struct Enigma {
-  wheels: Vec<Wheel>,
-  plugboard: HashMap<char, char>,
+    wheels: Vec<Wheel>,
+    plugboard: HashMap<char, char>,
 }
 
 impl Enigma {
-  fn setup(&mut self, file_path: &str) {
-      /* read the file */
-      let settings = YamlLoader::load_from_str(&*fs::read_to_string(file_path).expect("to read settings")).expect("to read settings");
-      /* load wheels */
-      let doc = &settings[0];
-      let wheels = &doc["wheels"];
-      for w in wheels.as_vec().unwrap() {
-        let wset = w.as_hash().unwrap();
-        let Some(name) = wset.get(&Yaml::from_str("name")).unwrap().as_str() else { panic!("Name fnot found"); };
-        let Some(pos) = wset.get(&Yaml::from_str("position")).unwrap().as_i64() else { panic!("Position not found"); } ;
-        let mut wheel = Wheel::by_name(name).unwrap();
-        wheel.step = pos as usize;
-        self.wheels.push(wheel);
-      }
-      let plugs = &doc["plugs"];
-      self.setup_plugboard(plugs)
-   }
+    fn setup(&mut self, file_path: &str) {
+        /* read the file */
+        let settings =
+            YamlLoader::load_from_str(&*fs::read_to_string(file_path).expect("to read settings"))
+                .expect("to read settings");
+        /* load wheels */
+        let doc = &settings[0];
+        let wheels = &doc["wheels"];
+        for w in wheels.as_vec().unwrap() {
+            let wset = w.as_hash().unwrap();
+            let Some(name) = wset.get(&Yaml::from_str("name")).unwrap().as_str() else {
+                panic!("Name fnot found");
+            };
+            let Some(pos) = wset.get(&Yaml::from_str("position")).unwrap().as_i64() else {
+                panic!("Position not found");
+            };
+            let mut wheel = Wheel::by_name(name).unwrap();
+            wheel.step = pos as usize;
+            self.wheels.push(wheel);
+        }
+        let plugs = &doc["plugs"];
+        self.setup_plugboard(plugs)
+    }
 
-   fn new() -> Enigma {
-     return Enigma { wheels: Vec::new(), plugboard: HashMap::new() };
-   }
+    fn new() -> Enigma {
+        Enigma {
+            wheels: Vec::new(),
+            plugboard: HashMap::new(),
+        }
+    }
 
     fn setup_plugboard(&mut self, plugs: &Yaml) {
-       match plugs.as_hash() {
-          Some(plugs)  => {
-            for p in plugs  {
-              let a = p.0.as_str().unwrap().chars().next().unwrap();
-              let b = p.1.as_str().unwrap().chars().next().unwrap();
-              self.plugboard.insert(a, b);
-              self.plugboard.insert(b, a); // Ensures bidirectional mapping
-           }
-         }
-         None => {},
-      }
-   }
+        // initialize with standard plugs
+        for c in ALPHA.chars() {
+            self.plugboard.insert(c, c);
+        }
+
+        match plugs.as_hash() {
+            Some(plugs) => {
+                for p in plugs {
+                    let a = p.0.as_str().unwrap().chars().next().unwrap();
+                    let b = p.1.as_str().unwrap().chars().next().unwrap();
+                    *self.plugboard.get_mut(&a).unwrap() = b;
+                    *self.plugboard.get_mut(&b).unwrap() = a; // Ensures bidirectional mapping
+                }
+            }
+            None => {}
+        }
+    }
 
     fn apply_plugboard(&self, c: char) -> char {
         *self.plugboard.get(&c).unwrap_or(&c)
     }
 
     fn step_wheels(&mut self) {
-      let mut step_next = true; // Rightmost rotor steps
-      let nwheels = self.wheels.len();
-      for i in 0..nwheels {
-        let wheel = &mut self.wheels[i];
-        if wheel.fixed { continue; } // Skip fixed wheels like the reflector
+        let mut step_next = true; // Rightmost rotor steps
+        let nwheels = self.wheels.len();
+        for i in 0..nwheels {
+            let wheel = &mut self.wheels[i];
+            if wheel.fixed {
+                continue;
+            } // Skip fixed wheels like the reflector
 
-        if step_next {
-          wheel.step = (wheel.step + 1) % ALPHA.len();
-          step_next = wheel.notch.contains(ALPHA.chars().nth(wheel.step).unwrap());
-        }
+            if step_next {
+                wheel.step = (wheel.step + 1) % ALPHA.len();
+                step_next = wheel.notch.contains(ALPHA.chars().nth(wheel.step).unwrap());
+            }
 
-        // Handle double-stepping anomaly
-        if i < nwheels - 1 && step_next && wheel.notch.contains(ALPHA.chars().nth(wheel.step).unwrap()) {
-          self.wheels[i + 1].step = (self.wheels[i + 1].step + 1) % ALPHA.len();
+            // Handle double-stepping anomaly
+            if i < nwheels - 1
+                && step_next
+                && wheel.notch.contains(ALPHA.chars().nth(wheel.step).unwrap())
+            {
+                self.wheels[i + 1].step = (self.wheels[i + 1].step + 1) % ALPHA.len();
+            }
         }
-      }
     }
 
     fn send(&mut self, mut c_in: char) -> char {
-      let mut c = c_in;
-      let mut wi: usize = 0;
-      let mut dir = 0;
+        let mut c = c_in;
+        let mut wi: usize = 0;
+        let mut dir = 0;
 
-      self.step_wheels();
+        self.step_wheels();
 
-      /* go through plug board */
-      c = self.apply_plugboard(c);
+        /* go through plug board */
+        c = self.apply_plugboard(c);
 
-      /* go through each loop */
-      loop {
-        c_in = c;
-        let Some(wheel) = self.wheels.get_mut(wi) else { panic!("Wheel index {} missing", wi) };
+        /* go through each loop */
+        loop {
+            c_in = c;
+            let Some(wheel) = self.wheels.get_mut(wi) else {
+                panic!("Wheel index {} missing", wi)
+            };
 
-        if dir == 0 {
-           /* going from stator -> reflector */
-           c = wheel.send_right(c_in);
-        } else {
-           /* going from reflector -> stator */
-           c = wheel.send_left(c_in);
+            if dir == 0 {
+                /* going from stator -> reflector */
+                c = wheel.send_right(c_in);
+            } else {
+                /* going from reflector -> stator */
+                c = wheel.send_left(c_in);
+            }
+
+            /* Choose next wheel */
+            if dir == 0 {
+                wi += 1;
+            } else {
+                if wi == 0 {
+                    break;
+                }
+                wi -= 1;
+            }
+
+            /* Reached reflector, need to use the one wheel before reflector */
+            if wi >= self.wheels.len() {
+                dir = 1;
+                wi -= 2;
+            }
         }
 
-        /* Choose next wheel */
-        if dir == 0 {
-          wi += 1;
-        } else {
-          if wi == 0 {
-            break;
-          }
-          wi -= 1;
+        /* Once more through plugboard */
+        c = self.apply_plugboard(c);
+
+        c
+    }
+
+    fn reset(&mut self) {
+        for wheel in &mut self.wheels {
+            wheel.step = 0;
         }
+    }
 
-        /* Reached reflector, need to use the one wheel before reflector */
-        if wi >= self.wheels.len() {
-          dir = 1;
-          wi -= 2;
+    fn encrypt(&mut self, plain: &str) -> String {
+        let mut result = String::from("");
+        for c in plain.to_uppercase().chars() {
+            match ALPHA.find(c) {
+                Some(_index) => result.push(self.send(c)),
+                None => result.push(c),
+            }
         }
-      }
-
-      /* Once more through plugboard */
-      c = self.apply_plugboard(c);
-
-      c
-   }
-
-   fn reset(&mut self) {
-      for wheel in &mut self.wheels {
-        wheel.step = 0;
-      }
-   }
-
-   fn encrypt(&mut self, plain: &str) -> String {
-     let mut result = String::from("");
-     for c in plain.to_uppercase().chars() {
-        match ALPHA.find(c) {
-           Some(_index) => result.push(self.send(c)),
-           None => result.push(c)
-        }
-     }
-     return result;
-   }
+        result
+    }
 }
 
 pub fn main() {
-  let mut enigma = Enigma::new();
-  let args: Vec<String> = env::args().collect();
-  enigma.setup(&args[2]);
-  let stdin =io::stdin();
-  for line in stdin.lock().lines() {
-    let cipher = enigma.encrypt(&line.unwrap());
-    println!("{cipher}");
-  }
+    let mut enigma = Enigma::new();
+    let args: Vec<String> = env::args().collect();
+    enigma.setup(&args[2]);
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        let cipher = enigma.encrypt(&line.unwrap());
+        println!("{cipher}");
+    }
 }
